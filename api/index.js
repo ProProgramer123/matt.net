4a569f8a83266f82a67ebe7d70dd66900418e253
ProProgramer123 <162527598+ProProgramer123@users.noreply.github.com>
2026-08-13 07:25:47 +0000

    more api fixes oml

--- api/index.js
+++ api/index.js
@@ -1,12 +1,7 @@
-module.exports = (req, res) => {
-  res.status(200);
-
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Cache-Control", "no-store");
-
-  res.end(JSON.stringify({
-    success: true,
-    version: 2,
-    service: "matt.net"
-  }));
+module.exports = function handler(req, res) {
+  res.status(200).json({
+    ok: true,
+    name: "matt-net",
+    version: 2
+  });
 };
